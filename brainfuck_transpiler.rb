21e63354ce97b2cd13d0c67ebdea6d36cfd4fc47
Yuri Kasperovich <ykas.gg@gmail.com>
2017-01-17 19:22:22 +0300

    Update brainfuck_transpiler.rb

--- brainfuck_transpiler.rb
+++ brainfuck_transpiler.rb
@@ -1,50 +1,50 @@
-class BrainfuckTtanspiler  
-  
+class BrainfuckTtanspiler
   def initialize
     @numbers = []
     @brainfuck_code = []
   end
   
   def get_brainfuck_code
-    input
-    find_a_conclusion
-    get_the_numbers_to_ASCII
-    convert_brainfuck_code
-    output_to_file
+    read_input_file
+    find_puts
+    convert_text_to_numbers
+    convert_numbers_to_brainfuck_code
+    write_output_file
   end
   
   private
   
   attr_reader :file, :numbers, :text, :brainfuck_code
 
-  def input
+  def read_input_file
     File.open(ARGV[0]) do |review_file|
       @file = review_file.read
     end
   end
       
-  def find_a_conclusion
+  def find_puts
     file =~ /^\s*puts\s["|']+?(.+)["|']+?\n/
     @text = $1.to_s.chars
   end
     
-  def get_the_numbers_to_ASCII
+  def convert_text_to_numbers
     text.each do |word|
       @numbers << word.ord
     end    
   end
   
-  def convert_brainfuck_code
+  def convert_numbers_to_brainfuck_code
     numbers.each do |number|
-      @brainfuck_code << "+" * number.to_i + "."            
+      @brainfuck_code << "+" * number + "."            
     end 
     
     @brainfuck_code = brainfuck_code.join
   end  
   
-  def output_to_file      
+  def write_output_file      
     File.open(ARGV[1], "w") {|files| files.write brainfuck_code}    
   end
 end
 
-BrainfuckTtanspiler.new.get_brainfuck_code
+transpiler = BrainfuckTtanspiler.new
+transpiler.get_brainfuck_code
